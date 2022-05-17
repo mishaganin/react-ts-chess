91bdfc3e96044348e137cbeafbf892e1f67f89ac
Misha Ganin <gma-13@yandex.ru>
2022-05-17 21:21:35 +0300

    feat: add board

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import './styles/App.css';
+import BoardComponent from './components/BoardComponent';
+import { Board } from './components/models/Board';
+
+const App = () => {
+  const [board, setBoard] = useState(new Board());
+
+  useEffect(() => {
+    restart();
+  }, [])
+
+  function restart() {
+    const newBoard = new Board();
+    newBoard.initCells();
+    setBoard(newBoard);
+  }
+
+  return (
+    <div className="app">
+      <BoardComponent board={board} setBoard={setBoard}/>
+    </div>
+  )
+}
+
+export default App;
